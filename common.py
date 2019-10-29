9785ef4bb9ee380d706880ddb43bce0ea3bd4a30
Chris Hooven <chooven@rushline.com>
2019-10-29 09:20:51 -0700

    move URL to a const

--- common.py
+++ common.py
@@ -1,10 +1,10 @@
 import requests
 from models import Counts
+import constants
 
 
 def data_request():
-    response = requests.get(
-        "https://gist.githubusercontent.com/joelbirchler/66cf8045fcbb6515557347c05d789b4a/raw/9a196385b44d4288431eef74896c0512bad3defe/exoplanets")
+    response = requests.get(constants.SITE_URL)
     if response.ok:
         return response.content
     else:
--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+SITE_URL = "https://gist.githubusercontent.com/joelbirchler/66cf8045fcbb6515557347c05d789b4a/raw/9a196385b44d4288431eef74896c0512bad3defe/exoplanets"
+
 PLANET_SIZE = {
     'UNKNOWN': 0,
     'SMALL': 1,
